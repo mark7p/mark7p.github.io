830448cab00da2bcf297dc1948dc8aa3a2e9b70f
Mon Paniterce <gen.paniterce@gmail.com>
2024-11-04 10:59:00 +0800

    fix layout

--- src/components/Home/ProjectsShowcase/ProjectCard/ProjectCard.tsx
+++ src/components/Home/ProjectsShowcase/ProjectCard/ProjectCard.tsx
@@ -11,16 +11,16 @@ export type ProjectCardProps = {
 
 export default function ProjectCard({project}: { project: ProjectCardProps }) {
     return (
-        <div className="project-card flex gap-5 p-5 rounded-lg hover:text-custom-teal-100 hover:bg-custom-teal-300/20 mb-3 hover:cursor-pointer select-none">
-            <div className="project-image rounded overflow-hidden min-w-[10rem] max-w-[20rem]">
+        <div className="project-card flex gap-5 p-5 rounded-lg  hover:text-custom-teal-100 hover:bg-custom-teal-300/20 mb-3 hover:cursor-pointer select-none flex-wrap">
+            <div className="project-image flex rounded overflow-hidden w-full h-min min-w-[8rem] max-w-[20rem]">
                 {project.image}
             </div>
-            <div className="project-info text-left min-w-[20rem] max-w-[30rem]">
+            <div className="flex flex-col project-info text-left min-w-[12rem] max-w-[30rem]">
                 <h2 className="text-xl pb-2 font-bold">{project.title}</h2>
-                <p className="text-md text-custom-teal-50/70">{project.description}</p>
-                <div className="tags-container flex gap-2 pt-3">
+                <p className="text-md w-full text-custom-teal-50/70 text-balance">{project.description}</p>
+                <div className="tags-container flex gap-2 pt-3 flex-wrap">
                     {project.tags.map((tag: string, i: number) => {
-                        return <span className="tag rounded-full p-2 text-custom-teal-100 bg-custom-teal-200/20 text-sm" key={i}>{tag}</span>
+                        return <span className="tag rounded-full py-1 px-2 whitespace-nowrap text-custom-teal-100 bg-custom-teal-200/20 text-sm" key={i}>{tag}</span>
                     })}
                 </div>
             </div>
--- src/components/Home/ProjectsShowcase/ProjectsShowcase.tsx
+++ src/components/Home/ProjectsShowcase/ProjectsShowcase.tsx
@@ -1,38 +1,52 @@
 import ProjectCard, { ProjectCardProps } from "./ProjectCard/ProjectCard";
-
+import stringnotesCreatrImg from "/src/assets/stringnotes-creatr.png";
+import stringnotesAnimatrImg from "/src/assets/stringnotes-animatr.png";
+import notecraftrImg from "/src/assets/notecraftr.png";
+import regextractrImg from "/src/assets/regextractr.png";
 
 const projects: ProjectCardProps[] = [
     {
         title: "Stringnotes Creatr",
         id: 1,
         description: "A powerful, easy-to-use lyric video creator for musicians and content creators. Stringnotes Creatr has seamless Adobe Premiere Pro integration allows for advanced customization, giving you complete creative control.",
-        image: <img src="/src/assets/notecraftr.png"/>,
+        image: <img src={stringnotesCreatrImg}/>,
         link: "https://example.com/project1",
-        tags: ["React", "React Native", "Typescript", "Python" , "Django"]
+        tags: ["React", "Redux", "React Native", "Typescript", "Python", "Django"]
     },
     {
         title: "NoteCraftr",
         id: 2,
         description: "A tool for professionals who want to streamline their note-taking process. Easily craft customized notes by toggling note sections. With the ability to create and activate templates containing your composed sections, NoteCraftr empowers you to tailor every note to fit your exact needs. Once your note is complete, simply copy the result with it's seamless clipboard integration.",
-        image: <img src="/src/assets/notecraftr.png"/>,
+        image: <img src={notecraftrImg}/>,
         link: "https://example.com/project1",
-        tags: ["Angular", "Tauri", "Typescript", "Rust", "SCSS"]
+        tags: ["Angular", "RXJS", "Redux", "Tauri", "Typescript", "SCSS"]
     },
     {
         title: "Regextractr",
         id: 3,
         description: "A chrome extension to search text in real-time across any dynamic website. Perfect for front-end developers, researchers, and productivity enthusiasts working in the ever-changing landscape of Single Page Applications (SPAs).",
-        image: <img src="/src/assets/notecraftr.png"/>,
+        image: <img src={regextractrImg}/>,
         link: "https://example.com/project1",
-        tags: ["Javascript", "CSS"]
+        tags: ["Angular", "Javascript", "CSS"]
     },
+    {
+        title: "Stringnotes Animatr",
+        id: 4,
+        description: "Animation Tool for my YouTube Channel that imports MIDI files and syncs a guitar playing animation, integrated with OBS.",
+        image: <img src={stringnotesAnimatrImg}/>,
+        link: "https://example.com/project1",
+        tags: ["Godot", "Python", "Photoshop", "Krita"]
+    }
 
     
 ];
 
 export default function ProjectShowcase() {
     return (
-        <div className="h-screen border-t gap-3 flex flex-col border-custom-teal-50/50 justify-start items-center">
+        // <div>
+            
+        // </div>
+        <div className="min-h-[100vh] border-t gap-3 flex flex-col border-custom-teal-50/50  items-center p-2">
             <h1 className="text-3xl font-bold">Things I’ve Built</h1>
             <h2 className="text-lg font-medium text-custom-teal-50/70">A Mix of Projects That Challenge and Inspire Me</h2>
             {
